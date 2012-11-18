04f8420cfeee9c1b0052e41f8a434781c31508a5
joey <joeyw@reallyenglish.com>
2012-11-18 02:01:16 +0000

    document root changed to app

--- testserver/runserver.sh
+++ testserver/runserver.sh
@@ -8,7 +8,7 @@ export ENV
 
 case "$1" in
     start)
-        "$HTTPD" -f "$ROOT/httpd.conf" -C "DocumentRoot $ROOT/../" -C "ErrorLog \"$ROOT/error_log\"" -C "CustomLog \"$ROOT/access_log\" combined" -C "PidFile $ROOT/pid" -C "LockFile $ROOT/lock" -C "PHP_Admin_Value include_path \"$ROOT/../\""
+        "$HTTPD" -f "$ROOT/httpd.conf" -C "DocumentRoot $ROOT/../app" -C "ErrorLog \"$ROOT/error_log\"" -C "CustomLog \"$ROOT/access_log\" combined" -C "PidFile $ROOT/pid" -C "LockFile $ROOT/lock" -C "PHP_Admin_Value include_path \"$ROOT/../\""
         rm -f lock.* pid
         ;;
     stop)
