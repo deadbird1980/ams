584e962907d223917ce0fbfd41ed5582d4992cdc
joey <joeyw@reallyenglish.com>
2012-11-21 22:14:24 +0000

    apply table changes to Model

--- app/protected/model/User.php
+++ app/protected/model/User.php
@@ -5,12 +5,16 @@ class User extends DooSmartModel{
 
     public $id;
     public $username;
-    public $pwd;
+    public $password;
     public $group;
     public $first_name;
     public $last_name;
     public $first_name_alphabet;
     public $last_name_alphabet;
+    public $home_address;
+    public $local_address;
+    public $birthday;
+    public $phone;
     public $_table = 'user';
     public $_primarykey = 'id';
     public $_fields = array('id','username','pwd','group','first_name','last_name','first_name_alphabet','last_name_alphabet','address');
