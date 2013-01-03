dabb87c50a96ec5422fedb972cd20afa436e6b62
joey <joeyw@reallyenglish.com>
2013-01-03 10:00:29 +0000

    scopeSeenByUser for Application added

--- app/protected/model/Application.php
+++ app/protected/model/Application.php
@@ -7,6 +7,7 @@ class Application extends DooSmartModel{
     public $user_id;
     public $type;
     public $status;
+    public $assignee_id;
     public $start_date;
     public $end_date;
     public $paid;
@@ -15,6 +16,17 @@ class Application extends DooSmartModel{
     public $_fields = array('id','user_id','type','status','start_date','end_date','paid');
     function __construct(){
         parent::$className = __CLASS__;     //a must if you are using static querying methods Application::_count(), Application::getById()
+    }
+
+    public function scopeSeenByUser($user) {
+        if ($user->isAdmin()) {
+            return array();
+        } elseif ($user->isCounselor() || $user->isExecutor()) {
+            return array('where'=>'assignee_id='.$user->id);
+        } elseif ($user->isCustomer()) {
+            return array('where'=>'user_id='.$user->id);
+        }
+        return array();
     }
 }
 ?>
