4bdb1461ceb58cdea0d18475a736c4aae5b2cf31
joey <joeyw@reallyenglish.com>
2013-01-04 18:00:05 +0000

    fix table name bug

--- app/protected/model/VisaApplication.php
+++ app/protected/model/VisaApplication.php
@@ -16,7 +16,7 @@ class VisaApplication extends DooSmartModel{
     public $visa_start_date;
     public $visa_end_date;
     public $address;
-    public $_table = 'user';
+    public $_table = 'visa_application';
     public $_primarykey = 'id';
     public $_fields = array('id','application_id','country','start_date','passport_no','passport_name','birthday','organization','passport_start_date','passport_end_date','visa_start_date','visa_end_date','address');
     function __construct(){
