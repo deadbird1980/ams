f8d5ee8bf0ceb041e94ade7fb9b39cf90b877435
joey <joeyw@reallyenglish.com>
2013-01-04 09:36:27 +0000

    application confirm page added

--- app/protected/controller/MyController.php
+++ app/protected/controller/MyController.php
@@ -199,6 +199,28 @@ class MyController extends BaseController {
         $this->renderAction('/my/application/files');
     }
 
+    public function confirmApplication() {
+        Doo::loadModel('Application');
+
+        $app = new Application();
+        $this->data['application'] = $app->getById_first($this->params['id']);
+
+        $form = $this->getConfirmApplicationForm();
+        if ($this->isPost() && $form->isValid($_POST)) {
+            $id = $this->params['id'];
+            $app = new Application();
+            $app = $app->getById_first($id);
+            $visaapp = new VisaApplication();
+            $visaapp = $visaapp->getById_first($id);
+            $app = new VisaApplication($_POST);
+            $app->id = $id;
+            $app->update_attributes($_POST, array('where'=>"id=${id}"));
+            return Doo::conf()->APP_URL . "index.php/my/applications/{$id}/files";
+        }
+        $this->data['form'] = $form->render();
+        $this->renderAction('/my/application/edit');
+    }
+
     private function getActivateUserForm() {
         Doo::loadHelper('DooForm');
         $action = Doo::conf()->APP_URL . 'index.php/my/users/activate';
@@ -358,6 +380,112 @@ class MyController extends BaseController {
                  )),
                  'submit' => array('submit', array(
                      'label' => "下一步",
+                     'attributes' => array('class' => 'buttons'),
+                     'order' => 100,
+                 'field-wrapper' => 'div'
+                 ))
+             )
+        ));
+        return $form;
+    }
+
+    private function getConfirmApplicationForm() {
+        Doo::loadHelper('DooForm');
+        $app = $this->data['application'];
+        if ($app) {
+            if ($app->id) {
+            $action = Doo::conf()->APP_URL . 'index.php/my/applications/'. $app->id;
+            } else {
+            $action = Doo::conf()->APP_URL . 'index.php/my/applications/create/'.$app->type;
+            }
+        } else {
+            $action = Doo::conf()->APP_URL . 'index.php/my/applications/create/'. $this->params['type'];
+        }
+        Doo::loadModel('VisaApplication');
+        $visaapp = new VisaApplication();
+        $visaapp = $visaapp->getById_first($app->id);
+        $form = new DooForm(array(
+             'method' => 'post',
+             'action' => $action,
+             'attributes'=> array('id'=>'form', 'name'=>'form', 'class'=>'Zebra_Form'),
+             'elements' => array(
+                 'type' => array('hidden', array(
+                     'value' => $app->type,
+                 )),
+                 'start_date' => array('display', array(
+                     'label' => 'Start Date:',
+                     'content' => $visaapp->start_date,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'passport' => array('display', array(
+                     'content' => '护照信息:',
+                 )),
+                 'passport_no' => array('display', array(
+                     'label' => '护照号码:',
+                     'content' => $visaapp->passport_no,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'passport_name' => array('display', array(
+                     'label' => '护照姓名:',
+                     'content' => $visaapp->passport_name,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'birthday' => array('display', array(
+                     'label' => '生日:',
+                     'content' => $visaapp->birthday,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'passport_start_date' => array('display', array(
+                     'label' => '护照生效期:',
+                     'content' => $visaapp->passport_start_date,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'passport_end_date' => array('display', array(
+                     'label' => '护照截止日期:',
+                     'content' => $visaapp->passport_end_date,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'visa' => array('display', array(
+                     'content' => '当前签证状态:',
+                 )),
+                 'address' => array('display', array(
+                     'label' => '英国地址:',
+                     'content' => $visaapp->address,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'visa_start_date' => array('display', array(
+                     'label' => '签证开始日期:',
+                     'content' => $visaapp->visa_start_date,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'visa_end_date' => array('display', array(
+                     'label' => '签证结束日期:',
+                     'content' => $visaapp->visa_end_date,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'work' => array('display', array(
+                     'content' => '目前工作学习状况:',
+                 )),
+                 'organization' => array('display', array(
+                     'label' => '公司/学校名称:',
+                     'content' => $visaapp->organization,
+                     'attributes' => array('class' => 'control'),
+                 'element-wrapper' => 'div'
+                 )),
+                 'files' => array('display', array(
+                     'content' => '文件:',
+                 )),
+                 'submit' => array('submit', array(
+                     'label' => "提交",
                      'attributes' => array('class' => 'buttons'),
                      'order' => 100,
                  'field-wrapper' => 'div'
