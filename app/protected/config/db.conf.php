a76b293a7f802ebee86acc6d22affd6ec11083cd
joey <joeyw@reallyenglish.com>
2013-01-06 00:34:14 +0000

    add map between application and attachment

--- app/protected/config/db.conf.php
+++ app/protected/config/db.conf.php
@@ -6,9 +6,14 @@
  **/
 
 //User relationship
-$dbmap['Application']['belongs_to']['User'] = array('foreign_key'=>'id');
 $dbmap['User']['has_many']['Application'] = array('foreign_key'=>'user_id');
+
+
+//Application relationship
+$dbmap['Application']['has_many']['Attachment'] = array('foreign_key'=>'application_id');
+$dbmap['Application']['belongs_to']['User'] = array('foreign_key'=>'id');
 //$dbmap['Food']['has_many']['Ingredient'] = array('foreign_key'=>'food_id', 'through'=>'food_has_ingredient');
+$dbmap['Attachment']['belongs_to']['Application'] = array('foreign_key'=>'id');
 
 $dbconfig['dev'] = array('localhost', 'ams_dev', 'root', '', 'mysql',true);
 ?>
