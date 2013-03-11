bcfa706484ccf9a4d35fa5b34fbeb10408be6272
joey <joeyw@reallyenglish.com>
2013-03-11 10:04:30 +0000

    add filter required for script to pick up the right element

--- app/protected/class/FileHelper.php
+++ app/protected/class/FileHelper.php
@@ -3,7 +3,7 @@ class FileHelper extends Helper {
 
     public function getFilesForApplication($app) {
         $files = $app->files();
-        $html = "<select name='application_file[]'>";
+        $html = "<select name='application_file[]' required>";
         foreach($files as $file) {
             $html .= "<option value={$file->id}>{$file->name}</option>";
         }
