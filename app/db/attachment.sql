d6a767edf50737a1324f172dec3cab7fe86a1647
joey <joeyw@reallyenglish.com>
2013-03-11 10:03:42 +0000

    appliction_file_id added for each file submission

--- app/db/attachment.sql
+++ app/db/attachment.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS `attachment`;
 CREATE TABLE `attachment` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `application_id` int(11) NOT NULL,
+  `application_file_id` int(11) NOT NULL,
   `file_name` char(50) DEFAULT NULL,
   `file_size` char(50) DEFAULT NULL,
   `file_type` char(50) DEFAULT NULL,
--- app/protected/model/Attachment.php
+++ app/protected/model/Attachment.php
@@ -5,13 +5,15 @@ class Attachment extends DooSmartModel{
 
     public $id;
     public $application_id;
+    public $application_file_id;
     public $file_name;
     public $file_size;
     public $file_type;
     public $title;
     public $_table = 'attachment';
     public $_primarykey = 'id';
-    public $_fields = array('id','application_id','file_name','file_size','file_type','title');
+    public $_fields = array('id','application_id','application_file_id','file_name','file_size','file_type','title');
+
     function __construct(){
         parent::$className = __CLASS__;     //a must if you are using static querying methods Application::_count(), Application::getById()
     }
@@ -20,6 +22,7 @@ class Attachment extends DooSmartModel{
         $this->file_name = $file->name;
         $this->file_size = $file->size;
         $this->file_type = $file->type;
+        $this->application_file_id = $file->application_file;
         return $this->insert();
     }
 
