0949cadf94d5f272c68dd292240b3ec24296bd5e
joey <joeyw@reallyenglish.com>
2013-03-16 13:07:33 +0000

    attachment and application_file relationship specified

--- app/protected/config/db.conf.php
+++ app/protected/config/db.conf.php
@@ -16,6 +16,8 @@ $dbmap['Application']['belongs_to']['Assignee'] = array('foreign_key'=>'id');
 $dbmap['Assignee']['has_many']['Application'] = array('foreign_key'=>'assignee_id');
 //$dbmap['Food']['has_many']['Ingredient'] = array('foreign_key'=>'food_id', 'through'=>'food_has_ingredient');
 $dbmap['Attachment']['belongs_to']['Application'] = array('foreign_key'=>'id');
+$dbmap['Attachment']['belongs_to']['ApplicationFile'] = array('foreign_key'=>'id');
+$dbmap['ApplicationFile']['has_many']['Attachment'] = array('foreign_key'=>'application_file_id');
 
 $dbconfig['dev'] = array('localhost', 'ams_dev', 'root', '', 'mysql',true);
 if (isset($_ENV['CLEARDB_DATABASE_URL'])) {
