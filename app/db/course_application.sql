ea94c0228b8a0b88e7fe58dce69097a1d48ef276
joey <joeyw@reallyenglish.com>
2013-04-02 23:08:25 +0100

    course application table added

---
+++ app/db/course_application.sql
@@ -0,0 +1,51 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4004
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.5.27)
+# Database: ams_dev
+# Generation Time: 2013-04-02 22:07:14 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table course_application
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `course_application`;
+
+CREATE TABLE `course_application` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `application_id` int(11) unsigned NOT NULL,
+  `school` text,
+  `subject` text,
+  `course` text,
+  `status` text,
+  `sent` datetime DEFAULT NULL,
+  `result` text,
+  `replied` datetime DEFAULT NULL,
+  `resent` datetime DEFAULT NULL,
+  `done` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
